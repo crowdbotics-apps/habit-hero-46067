c61c8865ffbfbdf6085c43cfb20e954be7cc6dac
Crowdbotics <team@crowdbotics.com>
2024-02-07 16:19:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "habit-hero-46067",
+  url: "https://habit-hero-46067.botics.co",
+  api: "https://habit-hero-46067.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
